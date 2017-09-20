be2eb08f7415cf0e5b5a89fcbfcf5c48a987c265
Minsu Park <minsupark@dhcp-uris-2038.redrover.cornell.edu>
2017-09-20 14:05:45 -0400

    updates

--- parse_CIA_worldfact.py
+++ parse_CIA_worldfact.py
@@ -16,23 +16,23 @@ for item in soup.find_all('tr'):
     country_id = item.get('id')
 
     if country_id != None:
-    	counter = 0
+        counter = 0
         for line in content.splitlines():
-        	if country_id not in d:
-        		d[country_id] = {}
-        	if counter == 0:
-        		country_name = line.strip()
-        		d[country_id]['country_name'] = country_name
-        		counter += 1
-        	else:
-        		age_g = line.strip().split()[0]
-        		d[country_id][age_g] = {}
-        		age_p = float(line.strip().split(': ')[1].split('%')[0])
-        		d[country_id][age_g]['proportion'] = age_p
-        		male_f = locale.atoi(line.strip().split('male ')[1].split('/')[0])
-        		d[country_id][age_g]['male'] = male_f
-        		female_f = locale.atoi(line.strip().split('female ')[1].split(')')[0])
-        		d[country_id][age_g]['female'] = female_f
+            if country_id not in d:
+                d[country_id] = {}
+            if counter == 0:
+                country_name = line.strip()
+                d[country_id]['country_name'] = country_name
+                counter += 1
+            else:
+                age_g = line.strip().split()[0]
+                d[country_id][age_g] = {}
+                age_p = float(line.strip().split(': ')[1].split('%')[0])
+                d[country_id][age_g]['proportion'] = age_p
+                male_f = locale.atoi(line.strip().split('male ')[1].split('/')[0])
+                d[country_id][age_g]['male'] = male_f
+                female_f = locale.atoi(line.strip().split('female ')[1].split(')')[0])
+                d[country_id][age_g]['female'] = female_f
 
 with open('./cia_factbook.json', 'w') as f:
     json.dump(d, f)
